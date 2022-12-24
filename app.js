f4d3197a095ba4ea19db95f41166aefd91229f2d
mcarlan <mgcarlan@gmail.com>
2022-12-23 19:47:45 -0600

    complete

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-console.log("Hello World!\n==========\n");
+let searchWord = document.getElementById("searchWord");
+let image = document.getElementById("image");
+let feedback = document.getElementById("feedback");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+let url = "https://api.giphy.com/v1/gifs/translate";
+let apiKey = "qbKdwndXqIJM4vEBQ9MxQZBzoJ8IBwfm";
+
+
+let submitSearch = document.getElementById("submitSearch");
+submitSearch.addEventListener("click",(event) => {
+    fetch(`${url}?api_key=${apiKey}&s=${searchWord.value}`).then((res) => {
+        return res.json();
+    }).then((gif) => {
+        console.log(gif);
+        image.src = gif.data.images.original.url;
+    }).catch((err) => {
+        console.error(err);
+        feedback.textContent = err.message; 
+    })
+
+    searchWord.value = "";
+}); 
+
+
+
+
+console.log(searchWord.value);
+
+
+
